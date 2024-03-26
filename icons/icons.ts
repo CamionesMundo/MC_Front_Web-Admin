2c95fcd6d12f1d8a80b5cfd1ef66c294519d1df7
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-03-25 23:58:35 -0500

    Feature/ Initial icons Added

---
+++ icons/icons.ts
@@ -0,0 +1,29 @@
+import { ReactComponent as Visible } from './src/Visible.svg'
+import { ReactComponent as Collapsable } from './src/Collapsable.svg'
+import { ReactComponent as Home } from './src/Home.svg'
+import { ReactComponent as Users } from './src/Users.svg'
+import { ReactComponent as ChevronRight } from './src/ChevronRight.svg'
+import { ReactComponent as LeftArrow } from './src/LeftArrow.svg'
+import { ReactComponent as Plus } from './src/Plus.svg'
+import { ReactComponent as XMarkCircle } from './src/XMarkCircle.svg'
+import { ReactComponent as CheckCircle } from './src/CheckCircle.svg'
+import { ReactComponent as Search } from './src/Search.svg'
+import { ReactComponent as Edit } from './src/Edit.svg'
+import { ReactComponent as Delete } from './src/Delete.svg'
+import { ReactComponent as Modules } from './src/Modules.svg'
+
+export {
+  Visible,
+  Collapsable,
+  Home,
+  Users,
+  ChevronRight,
+  LeftArrow,
+  Plus,
+  CheckCircle,
+  XMarkCircle,
+  Search,
+  Edit,
+  Delete,
+  Modules
+}
