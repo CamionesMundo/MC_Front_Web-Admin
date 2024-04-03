01d348d721cf5cb64189647357cf93e92e5c9aa0
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-03 11:28:04 -0500

    Fix/ Base url Backend

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -26,7 +26,7 @@ const authOptions: NextAuthOptions = {
         }
 
         const res = await fetch(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/users/login`,
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/admin/users/login`,
           {
             method: 'POST',
             body: JSON.stringify({ email, password }),
