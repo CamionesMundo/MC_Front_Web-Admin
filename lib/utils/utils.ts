cdbb95c5b4091c35c68c80383e7051747204ba1f
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:24:24 -0500

    Documentation/ Utils

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,8 +1,18 @@
 import { useRouter } from 'next/navigation'
+
+/**
+ * Capitalizes the first letter of a string.
+ * @param str The input string.
+ * @returns The input string with the first letter capitalized.
+ */
 export function capitalize (str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
+/**
+ * A hook to navigate back to the previous page using the router.
+ * @returns An object containing a function to handle going back to the previous page.
+ */
 export function useGoBackPage () {
   const router = useRouter()
   const handleGoBack = () => {
