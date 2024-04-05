8d5a6881f4528ecccc4e10e118e4763fd92ece38
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:35:56 -0500

    Fix/ Login view, removed react hook form dependency

--- app/(Auth)/login/page.tsx
+++ app/(Auth)/login/page.tsx
@@ -1,42 +1,70 @@
 'use client'
 import { GenericButton } from '@/components/buttons'
 import { CustomInput } from '@/components/inputs'
+import {
+  type FormErrorMessages,
+  handleValidationFormErrors
+} from '@/helpers/error'
 import { Logo, Visible } from '@/icons'
 import { loginSchema } from '@/lib/validators'
+import { useLoginStore, type LoginData } from '@/store/useLoginStore'
 import { useRecoveryPasswordStore } from '@/store/useRecoveryPasswordStore'
-import { zodResolver } from '@hookform/resolvers/zod'
+
 import { Link } from '@nextui-org/react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { type FormEvent, useState, type ChangeEvent } from 'react'
 import { toast } from 'sonner'
 
 const LoginPage = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    watch
-  } = useForm({
-    resolver: zodResolver(loginSchema)
-  })
   const router = useRouter()
+  // Custom hook to manage login state
+  const { email, password, setData } = useLoginStore()
+
+  // Custom hook to handle changing email for password recovery
   const { changeCurrentEmail } = useRecoveryPasswordStore()
+
+  // Local States for loading, control password visibility and form errors messages
   const [isLoading, setIsLoading] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
-  const emailValue = watch('email')
+  const [errors, setErrors] = useState<FormErrorMessages | null>(null)
 
+  /**
+   * Function to toggle password visibility.
+   */
   const togglePassword = () => {
     setShowPassword(!showPassword)
   }
 
-  const onSubmit = handleSubmit(async (data) => {
+  /**
+   * Function executed when submitting the login form.
+   * @param e Form event
+   */
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
     setIsLoading(true)
     try {
+      // Validate the email and password using the login schema
+      loginSchema.parse({ email, password })
+
+      // If validation succeeds, reset errors state to null
+      setErrors(null)
+    } catch (error) {
+      // If validation fails, handle the error and set errors state accordingly
+      const err = handleValidationFormErrors(error)
+      if (err !== undefined) {
+        setErrors(err)
+        setIsLoading(false)
+        return
+      }
+      setIsLoading(false)
+      return
+    }
+    // Attempt to sign in with provided credentials
+    try {
       const res = await signIn('credentials', {
-        email: data.email,
-        password: data.password,
+        email,
+        password,
         redirect: false
       })
 
@@ -58,14 +86,28 @@ const LoginPage = () => {
     }
 
     setIsLoading(false)
-  })
+  }
 
+  /**
+   * Function to initiate password recovery process.
+   */
   const onRecoveryPassword = () => {
-    const email = emailValue as string
-
     changeCurrentEmail(email)
   }
 
+  /**
+   * Function to handle form input change.
+   * @param e Input change event
+   * @param field Input field
+   */
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    field: keyof LoginData
+  ) => {
+    const newData: Partial<LoginData> = {}
+    newData[field] = e.target.value
+    setData(newData as LoginData)
+  }
   return (
     <div className='max-w-lg w-full bg-white rounded-2xl mx-auto mt-7 '>
       <div className='p-4 flex flex-col w-full'>
@@ -78,24 +120,30 @@ const LoginPage = () => {
         <form className='my-4' onSubmit={onSubmit}>
           <CustomInput
             name='email'
-            register={register}
+            value={email}
             type='text'
-            color={errors.email !== undefined ? 'danger' : 'primary'}
+            onChange={(e) => {
+              handleInputChange(e, 'email')
+            }}
+            color={errors?.email !== undefined ? 'danger' : 'primary'}
             label='Correo electrónico'
             placeholder='Ej. admin@mundocamiones.com'
-            error={errors.email?.message?.toString() ?? ''}
+            error={errors?.email?.toString() ?? ''}
           />
           <CustomInput
             name='password'
-            register={register}
+            value={password}
             type={showPassword ? 'text' : 'password'}
-            color={errors.password !== undefined ? 'danger' : 'primary'}
+            onChange={(e) => {
+              handleInputChange(e, 'password')
+            }}
+            color={errors?.password !== undefined ? 'danger' : 'primary'}
             label='Contraseña'
             placeholder='Ingresa aquí tu contraseña'
             endContent={
               <Visible className='icon-input' onClick={togglePassword} />
             }
-            error={errors.password?.message?.toString() ?? ''}
+            error={errors?.password?.toString() ?? ''}
           />
           <div className='flex justify-between pb-2'>
             <span className='text-sm'>Olvidé mi contraseña</span>
