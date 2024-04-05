5c72e0cb21d7431057cabec2e62b1b0c72e48cec
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:28:45 -0500

    Feature/ on Action function added in BackComponent

--- components/ui/BackComponent.tsx
+++ components/ui/BackComponent.tsx
@@ -5,15 +5,30 @@ import { useGoBackPage } from '@/lib/utils/utils'
 type BackComponentProps = {
   title: string
   subtitle: string
+  onAction?: () => void
 }
 
-const BackComponent = ({ title, subtitle }: BackComponentProps) => {
+/**
+ * Component for rendering a back button with title and subtitle.
+ * @param title The title to display.
+ * @param subtitle The subtitle to display.
+ * @param onAction Callback function to execute an action on button click.
+ */
+const BackComponent = ({ title, subtitle, onAction }: BackComponentProps) => {
+  /**
+   * Hook to handle navigating back to the previous page.
+   */
   const { handleGoBack } = useGoBackPage()
   return (
     <div className='  w-full flex justify-start items-center gap-4 flex-row'>
       <div
         className=' bg-transparent border-black/50 border-1 w-10 h-10 flex justify-center items-center rounded-lg hover:cursor-pointer hover:opacity-100 opacity-85'
-        onClick={handleGoBack}
+        onClick={() => {
+          handleGoBack()
+          if (onAction !== undefined) {
+            onAction()
+          }
+        }}
       >
         <LeftArrow className='w-4 h-4 text-black' />
       </div>
