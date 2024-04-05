271a047cc196556db8adf107f2ca07b44fe93c39
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:24:50 -0500

    Documentation/ Clsx

--- lib/clsx/clsx.ts
+++ lib/clsx/clsx.ts
@@ -1,6 +1,14 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Merges multiple class names and returns a single string representing the combined class names.
+ * @param inputs The class values to merge.
+ * @returns A string representing the combined class names.
+ */
 export function cn (...inputs: ClassValue[]) {
+  /**
+   * Merges multiple class names using the clsx library.
+   */
   return twMerge(clsx(inputs))
 }
