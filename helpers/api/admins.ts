314f7cf8b1cf51b25eba5fc3a1666e886bcdf2b8
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:40:46 -0500

    Feature/ Created admin helpers

--- helpers/api/admins.ts
+++ helpers/api/admins.ts
@@ -1,10 +1,55 @@
+import { BASE_ADMIN_URL } from '@/const/base-url'
 import api from '@/lib/axios/axios-client'
 import { type GenericResponse } from '@/types/api'
-import { type UserResponse } from '@/types/api/response/auth'
+import {
+  type BodyUpdateAdminForm,
+  type BodyAdminForm
+} from '@/types/api/request'
+import {
+  type UserDataResponse,
+  type UserResponse
+} from '@/types/api/response/auth'
+
 export const getAllAdmins = async () => {
   try {
-    const res = await api.get<GenericResponse<UserResponse[]>>(
-      '/admin/collaborators'
+    const res = await api.get<GenericResponse<UserResponse[]>>(BASE_ADMIN_URL)
+
+    const { data } = res
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getAdminById = async (id: number) => {
+  try {
+    const res = await api.get<GenericResponse<UserResponse>>(
+      `${BASE_ADMIN_URL}/id/${id}`
+    )
+
+    const { data } = res
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const createAdmin = async (body: BodyAdminForm) => {
+  const res = await api.post<GenericResponse<UserDataResponse>>(
+    BASE_ADMIN_URL,
+    body
+  )
+
+  const { data } = res
+
+  return data
+}
+
+export const updateAdmin = async (id: number, body: BodyUpdateAdminForm) => {
+  try {
+    const res = await api.put<GenericResponse<UserResponse>>(
+      `${BASE_ADMIN_URL}/id/${id}`,
+      body
     )
 
     const { data } = res
