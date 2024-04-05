2da4b1c79b2e3f75418d4a2bb46576101009b592
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:40:59 -0500

    Feature/ Created admin hooks

--- hooks/api/useAdmins.ts
+++ hooks/api/useAdmins.ts
@@ -1,9 +1,77 @@
-import { getAllAdmins } from '@/helpers/api/admins'
-import { useQuery } from '@tanstack/react-query'
+import {
+  createAdmin,
+  getAdminById,
+  getAllAdmins,
+  updateAdmin
+} from '@/helpers/api/admins'
+
+import { type RequestUpdateAdminForm } from '@/types/api/request'
+
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 export const useGetAllAdmins = () => {
   return useQuery({
     queryKey: ['administrators'],
     queryFn: async () => await getAllAdmins()
+  })
+}
+
+export const useGetAdminById = (id: number) => {
+  return useQuery({
+    queryKey: ['administrator', { id }],
+    queryFn: async () => await getAdminById(id),
+    enabled: id !== 0 && !isNaN(id)
+  })
+}
+
+export const useCreateAdmin = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['createAdmin'],
+    mutationFn: createAdmin,
+    onSuccess: async () => {
+      try {
+        await queryClient.invalidateQueries({ queryKey: ['administrators'] })
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      toast.error(data.message)
+    }
+  })
+}
+
+export const useUpdateAdmin = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['updateAdmin'],
+    mutationFn: async (request: RequestUpdateAdminForm) => {
+      return await updateAdmin(request.id, request.body)
+    },
+    onSuccess: async (response) => {
+      try {
+        await queryClient.invalidateQueries({ queryKey: ['administrators'] })
+        await queryClient.invalidateQueries({
+          queryKey: ['administrator', { id: response?.data.iduser_admin }]
+        })
+        router.refresh()
+        await queryClient.setQueryData(
+          ['administrator', { id: response?.data.iduser_admin }],
+          response?.data
+        )
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      toast.error(data.message)
+    }
   })
 }
