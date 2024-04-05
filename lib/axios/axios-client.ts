3a9ccd564edfa4ae52e4b882738e95b32895e9c0
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:26:03 -0500

    Documentation/  Axios Api Client

--- lib/axios/axios-client.ts
+++ lib/axios/axios-client.ts
@@ -9,14 +9,49 @@ axiosInstance.interceptors.request.use((config) => {
   return config
 })
 
+/**
+ * Function to create an API client using a given Axios instance.
+ * @param axiosInstance Axios instance to be used for making HTTP requests.
+ * @returns An object containing methods for making HTTP requests (GET, POST, PUT, DELETE) using the provided Axios instance.
+ */
+
 const api = (axiosInstance: AxiosInstance) => {
   return {
+    /**
+     * Sends a GET request to the specified URL.
+     * @param url The URL to send the request to.
+     * @param config Optional configuration for the request.
+     * @returns A promise resolving to the response data of type T.
+     */
     get: async <T>(url: string, config?: AxiosRequestConfig) =>
       await axiosInstance.get<T>(url, config),
+
+    /**
+     * Sends a POST request to the specified URL with the given body.
+     * @param url The URL to send the request to.
+     * @param body The data to send in the request body.
+     * @param config Optional configuration for the request.
+     * @returns A promise resolving to the response data of type T.
+     */
     post: async <T>(url: string, body: unknown, config?: AxiosRequestConfig) =>
       await axiosInstance.post<T>(url, body, config),
+
+    /**
+     * Sends a PUT request to the specified URL with the given body.
+     * @param url The URL to send the request to.
+     * @param body The data to send in the request body.
+     * @param config Optional configuration for the request.
+     * @returns A promise resolving to the response data of type T.
+     */
     put: async <T>(url: string, body: unknown, config?: AxiosRequestConfig) =>
       await axiosInstance.put<T>(url, body, config),
+
+    /**
+     * Sends a DELETE request to the specified URL.
+     * @param url The URL to send the request to.
+     * @param config Optional configuration for the request.
+     * @returns A promise resolving to the response data of type T.
+     */
     delete: async <T>(url: string, config?: AxiosRequestConfig) =>
       await axiosInstance.delete<T>(url, config)
   }
