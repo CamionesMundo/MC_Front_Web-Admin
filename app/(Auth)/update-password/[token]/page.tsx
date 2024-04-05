8b2b253a2bb10433beb80e0e044446b8a519c182
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-05 12:34:56 -0500

    Fix/ Update Password view, removed react hook form dependency

--- app/(Auth)/update-password/[token]/page.tsx
+++ app/(Auth)/update-password/[token]/page.tsx
@@ -3,43 +3,69 @@ import { GenericButton } from '@/components/buttons'
 import { CustomInput } from '@/components/inputs'
 import { Logo, Visible } from '@/icons'
 import { passwordSchema } from '@/lib/validators/passwordValidators'
-import { zodResolver } from '@hookform/resolvers/zod'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { type ChangeEvent, type FormEvent, useState } from 'react'
+
 import { useParams, useRouter } from 'next/navigation'
 import { useUpdatePassword } from '@/hooks/api/useAccount'
 import { type GenericResponse } from '@/types/api'
 import { showToast } from '@/hooks/useToast'
 import { type UserResponse } from '@/types/api/response/auth'
+import {
+  type UpdatePasswordData,
+  useUpdatePasswordStore
+} from '@/store/useUpdatePasswordStore'
+import {
+  handleValidationFormErrors,
+  type FormErrorMessages
+} from '@/helpers/error'
 
 type TypeParams = {
   token: string
 }
 const UpdatePasswordPage = () => {
+  const router = useRouter()
+
+  // Get parameters from the URL
   const params = useParams<TypeParams>()
   const token = params.token
+
+  // Custom hook to handle password update
   const { mutateAsync: sendNewPassword, isPending } = useUpdatePassword()
-  const router = useRouter()
+
+  // Custom hook to handle state of password update data
   const {
-    register,
-    handleSubmit,
-    formState: { errors }
-  } = useForm({
-    resolver: zodResolver(passwordSchema)
-  })
+    password,
+    confirmPassword,
+    setData,
+    togglePassword,
+    toggleConfirmPassword,
+    showPassword,
+    showConfirmPassword
+  } = useUpdatePasswordStore()
 
-  const [showPassword, setShowPassword] = useState(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+  const [errors, setErrors] = useState<FormErrorMessages | null>(null)
 
-  const togglePassword = () => {
-    setShowPassword(!showPassword)
-  }
-  const toggleConfirmPassword = () => {
-    setShowConfirmPassword(!showConfirmPassword)
-  }
-  const onSubmit = handleSubmit(async (data) => {
-    const { password } = data
+  // Function to handle form submission
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
     try {
+      // Validate the password and confirm password using the password schema
+      passwordSchema.parse({ password, confirmPassword })
+
+      // If validation succeeds, reset errors state to null
+      setErrors(null)
+    } catch (error) {
+      // If validation fails, handle the error and set errors state accordingly
+      const err = handleValidationFormErrors(error)
+      if (err !== undefined) {
+        setErrors(err)
+        return
+      }
+      return
+    }
+    try {
+      // Send new password to the server
       await sendNewPassword(
         { password, token },
         {
@@ -55,7 +81,55 @@ const UpdatePasswordPage = () => {
     } catch (error) {
       console.log(error)
     }
-  })
+  }
+
+  /**
+   * Handles change events on input fields for password update.
+   * Performs validation on the input value and updates the state accordingly.
+   * @param e The event triggered when the input field value changes.
+   * @param field The field being updated in the password data.
+   */
+  const handleInputChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    field: keyof UpdatePasswordData
+  ) => {
+    const newData: Partial<UpdatePasswordData> = {}
+    newData[field] = e.target.value
+
+    try {
+      // Validate the input field value using the password schema
+      passwordSchema.parse({ ...newData, [field]: e.target.value })
+
+      // If validation succeeds, update the errors state accordingly
+      setErrors((prevErrors) => {
+        if (prevErrors !== null && field in prevErrors) {
+          // Remove the field from the errors object if it exists
+          const { [field]: _, ...restErrors } = prevErrors
+          return restErrors
+        } else {
+          return prevErrors
+        }
+      })
+    } catch (error) {
+      const err = handleValidationFormErrors(error)
+      if (err !== undefined) {
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          [field]: err[field]
+        }))
+      }
+    }
+
+    setData(newData as UpdatePasswordData)
+  }
+
+  /**
+   * Handles blur events on input fields for password update.
+   * Performs validation on the input value and updates the state accordingly.
+   * @param e The event triggered when the input field loses focus.
+   * @param field The field being updated in the password data.
+   */
+
   return (
     <div className='max-w-lg w-full bg-white rounded-2xl mx-auto mt-7 '>
       <div className='p-4 flex flex-col w-full'>
@@ -75,27 +149,33 @@ const UpdatePasswordPage = () => {
         <form onSubmit={onSubmit}>
           <CustomInput
             name='password'
-            register={register}
+            value={password}
             type={showPassword ? 'text' : 'password'}
-            color={errors.password !== undefined ? 'danger' : 'primary'}
+            onChange={(e) => {
+              handleInputChange(e, 'password')
+            }}
+            color={errors?.password !== undefined ? 'danger' : 'primary'}
             label='Nueva    Contraseña'
             placeholder='Ingresa aquí tu contraseña'
             endContent={
               <Visible className='icon-input' onClick={togglePassword} />
             }
-            error={errors.password?.message?.toString() ?? ''}
+            error={errors?.password?.toString() ?? ''}
           />
           <CustomInput
             name='confirmPassword'
-            register={register}
+            value={confirmPassword}
+            onChange={(e) => {
+              handleInputChange(e, 'confirmPassword')
+            }}
             type={showConfirmPassword ? 'text' : 'password'}
-            color={errors.password !== undefined ? 'danger' : 'primary'}
+            color={errors?.password !== undefined ? 'danger' : 'primary'}
             label='Repetir Contraseña'
             placeholder='Ingresa aquí tu contraseña'
             endContent={
               <Visible className='icon-input' onClick={toggleConfirmPassword} />
             }
-            error={errors.confirmPassword?.message?.toString() ?? ''}
+            error={errors?.confirmPassword?.toString() ?? ''}
           />
           <div className='mt-3'>
             <GenericButton
