3557f019e31c36c56a646e057fd5f2d82a6ae3b7
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-10 08:31:15 -0500

    Fix/ Roles linter errors

--- app/(AdminPanel)/users-management/roles/page.tsx
+++ app/(AdminPanel)/users-management/roles/page.tsx
@@ -1,49 +1,8 @@
 'use client'
-import CustomTable from '@/components/table/CustomTable'
+
 import { BackComponent } from '@/components/ui/BackComponent'
-import { type Roles, rolesData } from '@/const/data'
-import React, { useEffect, useState } from 'react'
 
 const RolesPage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [data, setData] = useState<Roles[]>([])
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false)
-      setData(rolesData)
-    }, 2000)
-  }, [])
-  const columns = [
-    {
-      key: 'name',
-      display: 'Nombre',
-      render: (item: any) => item.name
-    },
-    {
-      key: 'canCreate',
-      display: 'Columna 1',
-      render: (item: any) => item.canCreate
-    },
-    {
-      key: 'canRead',
-      display: 'Columna 2',
-      render: (item: any) => item.canRead
-    },
-    {
-      key: 'canUpdate',
-      display: 'Columna 3',
-      render: (item: any) => item.canUpdate
-    },
-    {
-      key: 'canDelete',
-      display: 'columna 4',
-      render: (item: any) => item.canDelete
-    },
-    {
-      key: 'actions',
-      display: 'Acciones'
-    }
-  ]
   return (
     <div className='w-full bg-white/70 p-2 md:p-4 rounded-lg border border-gray-500/60'>
       <div className='w-full flex justify-start mb-2'>
@@ -54,17 +13,6 @@ const RolesPage = () => {
           Gestiona en esta sección los roles y permisos para cada colaborador.
         </p>
       </div>
-      <CustomTable
-        data={data}
-        columns={columns}
-        emptyLabel={isLoading ? '' : 'No tienes ningún rol creado'}
-        totalLabel='roles'
-        initialVisibleColumns={columns.map((column) => column.key)}
-        newButtonLabel={'Nuevo rol'}
-        onEdit={() => {}}
-        onDelete={() => {}}
-        isLoading={isLoading}
-      />
     </div>
   )
 }
