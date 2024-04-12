25195be8462d3fae48bfa9f2ca92018e7b823be4
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-11 19:37:29 -0500

    Feature/ enums file added

---
+++ types/enums.ts
@@ -0,0 +1,14 @@
+enum UserType {
+  Buyer = 1,
+  Seller = 2,
+  NaturalPerson = 3,
+  Business = 4,
+  LegalRepresentative = 5
+}
+
+enum Sex {
+  Hombre = 'Hombre',
+  Mujer = 'Mujer',
+}
+
+export { UserType, Sex }
