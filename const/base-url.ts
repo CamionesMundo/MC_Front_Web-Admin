d7f5354a75ab23baba4e4bbdcb28c136948fc831
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-11 19:41:29 -0500

    Feature/ New base Urls added

--- const/base-url.ts
+++ const/base-url.ts
@@ -1,3 +1,12 @@
 const BASE_ADMIN_URL = '/admin/collaborators'
-const BASE_MC_ADMIN_URL = '/auth/admin/users'
-export { BASE_ADMIN_URL, BASE_MC_ADMIN_URL }
+const BASE_CLIENT_URL = '/admin/clients'
+const BASE_MC_AUTH_URL = '/auth/admin'
+const BASE_MC_ADMIN_URL = `${BASE_MC_AUTH_URL}/users`
+const BASE_MC_CLIENT_URL = `${BASE_MC_AUTH_URL}/users/app/movil`
+export {
+  BASE_ADMIN_URL,
+  BASE_MC_ADMIN_URL,
+  BASE_MC_AUTH_URL,
+  BASE_CLIENT_URL,
+  BASE_MC_CLIENT_URL
+}
