a48b42e342c6433f9cc0568140444312ff10ed9f
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-19 12:10:55 -0500

    Feature/ New Functions in utils

--- lib/utils/utils.ts
+++ lib/utils/utils.ts
@@ -1,4 +1,4 @@
-import { UserType } from '@/types/enums'
+import { LanguagePreference, UserType } from '@/types/enums'
 import { useRouter } from 'next/navigation'
 
 /**
@@ -24,6 +24,11 @@ export function useGoBackPage () {
   }
 }
 
+/**
+ * Parses an ISO format date string to a localized date string.
+ * @param isoStringDate - The ISO format date string to parse.
+ * @returns The parsed date string formatted according to the locale.
+ */
 export function parseIsoDate (isoStringDate: string | null | undefined): string {
   if (isoStringDate === null || isoStringDate === undefined) return ''
   const date = new Date(isoStringDate)
@@ -39,6 +44,11 @@ export function parseIsoDate (isoStringDate: string | null | undefined): string 
   return date.toLocaleString('es-ES', options).replace('.', '')
 }
 
+/**
+ * Retrieves the user type as a string based on the provided user type code.
+ * @param userType - The user type code.
+ * @returns The user type as a string.
+ */
 export function getUserType (userType: number): string {
   switch (userType) {
     case UserType.Buyer:
@@ -56,9 +66,56 @@ export function getUserType (userType: number): string {
   }
 }
 
+/**
+ * Retrieves the language preference as a string based on the provided language preference code.
+ * @param language - The language preference code.
+ * @returns The language preference as a string.
+ */
+export function getLanguageString (
+  language: LanguagePreference | null | undefined
+): string {
+  switch (language) {
+    case LanguagePreference.English:
+      return 'Inglés'
+    case LanguagePreference.Spanish:
+      return 'Español'
+    default:
+      return 'Idioma no configurado'
+  }
+}
+
+/**
+ * Formats a date string to display only the day and month in a localized format.
+ * @param dateString - The date string to format.
+ * @returns The formatted date string.
+ */
 export function getBirthday (dateString: string | null | undefined): string {
   if (dateString === null || dateString === undefined) return 'No registrado'
   const date = new Date(dateString)
   const options = { day: '2-digit', month: 'long' } as const
   return new Intl.DateTimeFormat('es-ES', options).format(date)
+}
+
+/**
+ * Converts a date string to an ISO format date string "yyyy-MM-dd".
+ * Returns undefined if the date is null or undefined.
+ *
+ * @param dateString - The date in string format.
+ * @returns The date in "yyyy-MM-dd" format or undefined if the input is invalid.
+ */
+export function formatDateToYMD (dateString: string) {
+  if (dateString === undefined) {
+    return undefined
+  }
+
+  const date = new Date(dateString)
+  return isNaN(date.getTime()) ? undefined : date.toISOString().split('T')[0]
+}
+
+export function formatDateForInputDate (value: string) {
+  const dateParts = value.split('-')
+  const year = parseInt(dateParts[0])
+  const month = parseInt(dateParts[1]) - 1
+  const day = parseInt(dateParts[2])
+  return new Date(year, month, day)
 }
