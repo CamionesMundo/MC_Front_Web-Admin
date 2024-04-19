b2bdaaab54bb245b189a533b5d6c337db93d449c
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-19 12:12:27 -0500

    Feature/ users app routes added

---
+++ app/api/admin/clients/addresses/user/[id]/route.ts
@@ -0,0 +1,31 @@
+import { BASE_MC_ADDRESS } from '@/const/base-url'
+import { handleServerError } from '@/helpers/error'
+import mcApi from '@/lib/axios/mc-client'
+import { type GenericResponse } from '@/types/api'
+import { type AddressResponse } from '@/types/api/response/address'
+import { type AxiosResponse } from 'axios'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function GET (req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+  const id = pathname.split('/').pop()
+
+  try {
+    const mcInstance = await mcApi()
+
+    const res: AxiosResponse<GenericResponse<AddressResponse>> =
+      await mcInstance.get<GenericResponse<AddressResponse>>(
+        `${BASE_MC_ADDRESS}/all-address-user/${id}`
+      )
+
+    return NextResponse.json(res.data)
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({
+      statusCode: 500,
+      message: null,
+      data: null,
+      error: handleServerError(error)
+    })
+  }
+}
--- app/api/admin/clients/id/[id]/route.ts
+++ app/api/admin/clients/id/[id]/route.ts
@@ -2,7 +2,14 @@ import { BASE_MC_CLIENT_URL } from '@/const/base-url'
 import { handleServerError } from '@/helpers/error'
 import mcApi from '@/lib/axios/mc-client'
 import { type GenericResponse } from '@/types/api'
-import { type ClientResponse } from '@/types/api/response/user'
+import {
+  type BodyUpdateUser,
+  type BodyActiveUserApp
+} from '@/types/api/request/client-form'
+import {
+  type UserClientResponse,
+  type ClientResponse
+} from '@/types/api/response/user'
 import { type AxiosResponse } from 'axios'
 import { NextResponse, type NextRequest } from 'next/server'
 
@@ -27,5 +34,47 @@ export async function GET (req: NextRequest) {
       data: null,
       error: handleServerError(error)
     })
+  }
+}
+
+export async function PATCH (req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+  const id = pathname.split('/').pop()
+  const body = (await req.json()) as BodyActiveUserApp
+  try {
+    const mcInstance = await mcApi()
+
+    const res: AxiosResponse<GenericResponse<UserClientResponse>> =
+      await mcInstance.patch<GenericResponse<UserClientResponse>>(
+        `${BASE_MC_CLIENT_URL}/${id}`,
+        { active: body.active }
+      )
+
+    return NextResponse.json(res.data)
+  } catch (error) {
+    console.log(error)
+    return handleServerError(error)
+  }
+}
+
+export async function PUT (req: NextRequest) {
+  const pathname = req.nextUrl.pathname
+  const id = pathname.split('/').pop()
+  const body = (await req.json()) as BodyUpdateUser
+
+  const { data } = body
+  try {
+    const mcInstance = await mcApi()
+
+    const res: AxiosResponse<GenericResponse<UserClientResponse>> =
+      await mcInstance.put<GenericResponse<UserClientResponse>>(
+        `${BASE_MC_CLIENT_URL}/${id}`,
+        data
+      )
+
+    return NextResponse.json(res.data)
+  } catch (error) {
+    console.log(error)
+    return handleServerError(error)
   }
 }
