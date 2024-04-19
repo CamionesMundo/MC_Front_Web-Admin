2a8ce72430ac399420c694a5a39be57342c768af
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-19 12:26:20 -0500

    Feature/ Users Page added

--- app/(AdminPanel)/users-management/clients/page.tsx
+++ app/(AdminPanel)/users-management/clients/page.tsx
@@ -1,7 +1,29 @@
 'use client'
 
+import Users from '@/components/features/users-management/clients/Users'
+import { MainContainer } from '@/components/ui'
+import { handleClientError } from '@/helpers/error'
+import { useGetAllAppUsers } from '@/hooks/api/useClients'
+import { useMemo } from 'react'
+
 const ClientsPage = () => {
-  return <div>Clients</div>
+  const { data: clientsResponse, isLoading } = useGetAllAppUsers()
+  const clients = useMemo(() => {
+    const usersAdmin = clientsResponse?.data?.map((client) => ({
+      ...client,
+      id: client.iduser
+    }))
+    return usersAdmin
+  }, [clientsResponse])
+
+  if (clientsResponse?.error !== null && clientsResponse !== undefined) {
+    handleClientError(clientsResponse?.statusCode)
+  }
+  return (
+    <MainContainer>
+      <Users isLoading={isLoading} clients={clients ?? []} />
+    </MainContainer>
+  )
 }
 
 export default ClientsPage
