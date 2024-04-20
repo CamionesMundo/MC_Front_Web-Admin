1eb824e6a9a36762752973979d481ce352ddfd9f
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-20 02:18:42 -0500

    Fix/ Protected routes in matcher middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/', '/users-management/:path*']
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|login|recovery-password).*)'
+  ]
 }
