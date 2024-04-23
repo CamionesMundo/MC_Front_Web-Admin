2b97f7c8540b3f6ff418af48f59ae404f544cd66
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-23 17:26:03 -0500

    Feature/ Global validators added

---
+++ lib/validators/globalValidator.ts
@@ -0,0 +1,38 @@
+import { MAX_LENGTH, MAX_LENGTH_MESSAGE } from '@/const/other'
+import { z } from 'zod'
+import { REGEX_NUMBER_PHONE } from './regex'
+
+export const phoneSchema = z
+  .string()
+  .min(8, { message: 'Mínimo 8 caracteres' })
+  .max(MAX_LENGTH, { message: MAX_LENGTH_MESSAGE })
+  .refine((value) => REGEX_NUMBER_PHONE.test(value.trim()), {
+    message: 'Sólo espacios y números'
+  })
+
+export const floatNumberSchema = z
+  .string()
+  .refine((val) => !isNaN(parseFloat(val)), {
+    message: 'El valor debe ser un número'
+  })
+  .refine((val) => parseFloat(val) > 0, {
+    message: 'El valor debe ser mayor a cero'
+  })
+
+export const percentNumberSchema = z
+  .string()
+  .refine((val) => !isNaN(parseFloat(val)), {
+    message: 'El valor debe ser un número'
+  })
+  .refine((val) => parseFloat(val) > 0, {
+    message: 'El valor debe ser mayor a cero'
+  })
+  .refine((val) => parseFloat(val) < 101, {
+    message: 'El valor debe ser menor a 100'
+  })
+
+export const booleanSchema = z
+  .string()
+  .refine((val) => !isNaN(parseInt(val)), {
+    message: 'El valor debe ser un número'
+  })
