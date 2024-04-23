00e5c5a54641f080a5642a495bb8cc418907bbe2
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-23 17:30:11 -0500

    Feature/ New types added

---
+++ types/store/app-config.ts
@@ -0,0 +1,16 @@
+export type AppConfigFormData = {
+  warranty: string
+  mechanical: string
+  warrantyDeposit: string
+  maximumNumberPhotos: string
+  minimumNumberPhotos: string
+  commissionForSale: string
+  planMonthly: string
+  planAnual: string
+  freeMemberShipsForever: boolean
+  publicationFree: string
+  customAgentDescription: string
+  timeOfMarginForPayment: number
+  supportContactNumber: string
+  defaultAdmin: string
+}
