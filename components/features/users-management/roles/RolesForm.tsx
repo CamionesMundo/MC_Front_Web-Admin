451b512dcf4f4e723e0f2654e45d2f62c7baa318
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-04-29 10:27:43 -0500

    Feature/ Roles Form

--- components/features/users-management/roles/RolesForm.tsx
+++ components/features/users-management/roles/RolesForm.tsx
@@ -1,43 +1,72 @@
 import { CustomInput, GenericButton } from '@/components'
 import { BackComponent } from '@/components/ui/BackComponent'
 import { handleValidationErrors, type FormErrorMessages } from '@/helpers/error'
-import { useCreateRol, useGetAllPermissions } from '@/hooks/api/useRoles'
+import {
+  useCreateRol,
+  useGetAllPermissions,
+  useGetAllRoles,
+  useGetPermissionsByIdRole
+} from '@/hooks/api/useRoles'
 import { usePermissionFormStore } from '@/store/usePermissionsForm'
 import { Divider, Spacer } from '@nextui-org/react'
-import { useSession } from 'next-auth/react'
-// import { useParams, useRouter } from 'next/navigation'
-import React, { useMemo, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import React, { useEffect, useMemo, useState } from 'react'
 import SwitchPermission from './SwitchPermission'
 import { Loader } from '@/components/ui/Loader'
 import { nameSchema } from '@/lib/validators/globalValidator'
 import { type BodyRoleForm } from '@/types/api/request/roles-form'
 import { type GenericResponse } from '@/types/api'
-import { type PermissionCreateResponse } from '@/types/api/response/roles'
+import { type RolePermissionResponse } from '@/types/api/response/roles'
 import { showToast } from '@/hooks/useToast'
-import { useRouter } from 'next/navigation'
 type RolesFormProps = {
   isEditing?: boolean
 }
-// type TypeParams = {
-//   id?: string
-// }
+type TypeParams = {
+  id?: string
+}
 
 const RolesForm = ({ isEditing = false }: RolesFormProps) => {
-  const { data: session } = useSession()
+  const { data: rolesResponse } = useGetAllRoles()
   const { mutateAsync: createRol, isPending: isLoadingCreateRol } =
     useCreateRol()
-  console.log(session)
+
   const { data: permissionsResponse, isLoading: isLoadingPermissions } =
     useGetAllPermissions()
-  console.log(permissionsResponse)
+
   const router = useRouter()
-  // const params = useParams<TypeParams>()
-  // const id = params?.id ?? 0
+  const params = useParams<TypeParams>()
+  const id = params?.id ?? 0
+  const { data: permissionsByRole } = useGetPermissionsByIdRole(Number(id))
+  const permissionsToMatch = useMemo(() => {
+    const permissions = permissionsByRole?.data?.map(
+      (permission) => permission.permission.name_permission
+    )
+    if (permissions !== undefined) {
+      return permissions
+    } else {
+      return []
+    }
+  }, [permissionsByRole])
+
   const { setData, reset, ...permissions } = usePermissionFormStore()
-  // const [roleId, setRoleId] = useState<number | null>(null)
+
   const [errors, setErrors] = useState<FormErrorMessages | null>(null)
   const [name, setName] = useState<string>('')
 
+  useEffect(() => {
+    if (id !== 0 && rolesResponse !== undefined) {
+      const data = rolesResponse.data
+      const filtered = data.find(role => role.idrole_admin === Number(id))
+      setName(filtered?.name_role ?? '')
+    }
+  }, [id, rolesResponse])
+  useEffect(() => {
+    if (permissionsToMatch.length > 0) {
+      permissionsToMatch.forEach((permission) => {
+        setData(permission, true)
+      })
+    }
+  }, [permissionsToMatch, setData])
   const permissionsDB = useMemo(() => {
     if (permissionsResponse !== undefined) {
       return permissionsResponse.data
@@ -45,14 +74,13 @@ const RolesForm = ({ isEditing = false }: RolesFormProps) => {
       return []
     }
   }, [permissionsResponse])
-  console.log(permissionsDB)
+
   const onBack = () => {
     setTimeout(() => {
-      //   reset()
+      reset()
     }, 300)
   }
   const onSubmit = async () => {
-    console.log(name)
     try {
       nameSchema.parse(name)
       setErrors(null)
@@ -63,27 +91,69 @@ const RolesForm = ({ isEditing = false }: RolesFormProps) => {
         return
       }
     }
-    const filteredPermissions = permissionsDB.filter(
-      (permission) => permissions[permission.name_permission]
-    )
-    const mapIdsPermissions = filteredPermissions.map((permission) => ({
-      idpermission_admin: permission.idpermission_admin,
-      status: true
-    }))
+    if (isEditing) {
+      const allPermissions = permissionsDB.map((permission) => ({
+        idpermission_admin: permission.idpermission_admin,
+        status: permissions[permission.name_permission]
+      }))
 
-    const dataToCreate: BodyRoleForm = {
-      roleData: {
-        name_role: name
-      },
-      permissionsData: mapIdsPermissions
+      const dataToCreate: BodyRoleForm = {
+        roleData: {
+          name_role: name
+        },
+        permissionsData: allPermissions,
+        isEditing: true,
+        idRole: Number(id)
+      }
+
+      await onUpdate(dataToCreate)
+    } else {
+      const filteredPermissions = permissionsDB.filter(
+        (permission) => permissions[permission.name_permission]
+      )
+      const mapIdsPermissions = filteredPermissions.map((permission) => ({
+        idpermission_admin: permission.idpermission_admin,
+        status: true
+      }))
+
+      const dataToCreate: BodyRoleForm = {
+        roleData: {
+          name_role: name
+        },
+        permissionsData: mapIdsPermissions,
+        isEditing: false,
+        idRole: null
+      }
+      await onCreate(dataToCreate)
     }
-    await onCreate(dataToCreate)
   }
 
   const onCreate = async (updateData: BodyRoleForm) => {
     await createRol(updateData, {
       onSuccess: (
-        data: GenericResponse<PermissionCreateResponse[]> | undefined
+        data: GenericResponse<RolePermissionResponse> | undefined
+      ) => {
+        if (data?.error !== undefined) {
+          showToast(data.message, 'error')
+        } else {
+          showToast(data?.message ?? '', 'success')
+          router.refresh()
+          router.push('/users-management/roles')
+          setTimeout(() => {
+            reset()
+          }, 600)
+        }
+      },
+      onError: (data: Error) => {
+        showToast(data.message, 'error')
+      }
+    })
+  }
+
+  const onUpdate = async (updateData: BodyRoleForm) => {
+    await createRol(updateData, {
+      onSuccess: (
+        data: GenericResponse<RolePermissionResponse> | undefined
       ) => {
         if (data?.error !== undefined) {
           showToast(data.message, 'error')
@@ -160,8 +230,8 @@ const RolesForm = ({ isEditing = false }: RolesFormProps) => {
               type='button'
               label={isEditing ? 'Actualizar datos' : 'Crear rol'}
               onClick={onSubmit}
-              disabled={isEditing ? false : isLoadingCreateRol}
-              isLoading={isEditing ? false : isLoadingCreateRol}
+              disabled={ isLoadingCreateRol}
+              isLoading={ isLoadingCreateRol}
             />
           </div>
         </>
