64312b98c33867d816d417c9c828f022e8e003b2
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-05-03 15:04:48 -0500

    Feature/ New types response added

--- types/api/response/files.ts
+++ types/api/response/files.ts
@@ -9,3 +9,20 @@ export type FilesInfo = {
   idgallery: number
   name: string
 }
+
+export type FileGalleryResponse = {
+  idfile: number
+  url: string
+  thumbnail: null
+  main_file: boolean
+  idgallery: number
+  name: string
+  status: boolean
+  createdAt: Date
+  updatedAt: Date
+}
+
+export type FilesGalleryResponse = {
+  idgallery: number
+  files: FileGalleryResponse[]
+}
