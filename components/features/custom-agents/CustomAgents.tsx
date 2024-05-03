9b3af003c9cfb16e0c7a915696d59aba5e7dfcfe
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-05-03 15:20:20 -0500

    Feature/ Custom Agent component added

--- components/features/custom-agents/CustomAgents.tsx
+++ components/features/custom-agents/CustomAgents.tsx
@@ -1,9 +1,14 @@
+import { GenericButton } from '@/components'
+import CustomModal from '@/components/modal/CustomModal'
 import CustomTable from '@/components/table/CustomTable'
 import { BackComponent } from '@/components/ui/BackComponent'
 import { customAgentsColumns } from '@/const/columns/users'
+import { Edit } from '@/icons'
 import { type AgentDataType } from '@/types/api/response/custom-agents'
+import { useDisclosure } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
 import React, { useCallback, useMemo, useState } from 'react'
+import AgentDetails from './AgentDetails'
 
 type CustomAgentsProps = {
   agents: AgentDataType[]
@@ -12,6 +17,8 @@ type CustomAgentsProps = {
 
 const CustomAgents = ({ agents, isLoading }: CustomAgentsProps) => {
   const router = useRouter()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [currentIdAgent, setCurrentIdAgent] = useState<number | null>(null)
   const [filterValue, setFilterValue] = useState('')
 
   const hasSearchFilter = Boolean(filterValue)
@@ -48,6 +55,15 @@ const CustomAgents = ({ agents, isLoading }: CustomAgentsProps) => {
 
     return []
   }, [agents, filterValue, hasSearchFilter])
+
+  const gotoEditAgent = (id: number) => {
+    router.push(`/customs-agents/edit/id/${id}`)
+  }
+
+  const handleOnMoreDetails = (id: number) => {
+    setCurrentIdAgent(id)
+    onOpen()
+  }
   return (
     <>
       <div className='w-full flex justify-start mb-2'>
@@ -55,7 +71,8 @@ const CustomAgents = ({ agents, isLoading }: CustomAgentsProps) => {
       </div>
       <div>
         <p className='text-xs mb-2 dark:text-white'>
-          Gestiona en esta sección los datos de los agentes aduaneros asociados a Mundo Camiones.
+          Gestiona en esta sección los datos de los agentes aduaneros asociados
+          a Mundo Camiones.
         </p>
       </div>
       <CustomTable<AgentDataType>
@@ -66,9 +83,12 @@ const CustomAgents = ({ agents, isLoading }: CustomAgentsProps) => {
         columns={customAgentsColumns}
         emptyLabel={isLoading ? '' : 'No tienes ningún agente aduanero creado'}
         totalLabel='agentes aduaneros'
-        initialVisibleColumns={customAgentsColumns.map((column) => column.key).filter((key) => key !== 'updatedAt')}
+        initialVisibleColumns={customAgentsColumns
+          .map((column) => column.key)
+          .filter((key) => key !== 'updatedAt')}
         newButtonLabel={'Nuevo Agente'}
         onEdit={onEditRole}
+        onViewMore={handleOnMoreDetails}
         onDelete={() => {}}
         isLoading={isLoading}
         actionOnAdd={onCreateRole}
@@ -78,6 +98,29 @@ const CustomAgents = ({ agents, isLoading }: CustomAgentsProps) => {
           useDelete: true
         }}
       />
+
+      <CustomModal
+        titleModal={'Detalles del Agente aduanero'}
+        isOpen={isOpen}
+        onClose={onClose}
+        size='5xl'
+        footer={
+          <div className='w-full flex justify-end'>
+            <div className='md:w-1/4 sm:w-1/2 w-full'>
+              <GenericButton
+                type='button'
+                label={'Editar datos'}
+                startContent={<Edit className='w-3.5 h-3.5' />}
+                onClick={() => {
+                  gotoEditAgent(currentIdAgent ?? 0)
+                }}
+              />
+            </div>
+          </div>
+        }
+      >
+        <AgentDetails currentIdAgent={currentIdAgent}/>
+      </CustomModal>
     </>
   )
 }
