d1ca4512319b82ae25521eae43081c2fd516158f
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-05-29 17:01:04 -0500

    Feature/ New actions added for TableActions component

--- components/table/render-cell/TableActions.tsx
+++ components/table/render-cell/TableActions.tsx
@@ -1,6 +1,9 @@
 import { Delete, Edit, Visible } from '@/icons'
+import { cn } from '@/lib/clsx/clsx'
+import { type PaymentsDataType } from '@/types/api/response/payments'
+import { LotStatus } from '@/types/enums'
 import { type ActionsPermissions } from '@/types/ui/table'
-import { Tooltip } from '@nextui-org/react'
+import { Button, Tooltip } from '@nextui-org/react'
 import React from 'react'
 
 /**
@@ -24,11 +27,33 @@ type Props = {
   onViewMore: ((id: number) => void) | undefined
   onEdit: ((id: number) => void) | undefined
   onDelete: ((id: number) => void) | undefined
+  onConfirmPayment?: ((row: PaymentsDataType) => void) | undefined
+  onDetailPayment?: ((row: PaymentsDataType) => void) | undefined
   id: number
   actions: ActionsPermissions
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  row: any
 }
 
-const TableActions = ({ onViewMore, onEdit, onDelete, id, actions }: Props) => {
+const TableActions = ({
+  onViewMore,
+  onEdit,
+  onDelete,
+  onConfirmPayment,
+  onDetailPayment,
+  id,
+  actions = {
+    useDelete: true,
+    useEdit: true,
+    useViewMore: true,
+    isPaymentActions: false
+  },
+  row
+}: Props) => {
+  const isLot = Boolean(row.lot_code ?? false)
+  const isInprogress = isLot ? row.status === LotStatus.InProgress : false
+  const isPendingPayment = Boolean(row.payment_status ?? false)
+
   return (
     <div className='relative flex items-center justify-center gap-4'>
       {actions.useViewMore && (
@@ -46,28 +71,54 @@ const TableActions = ({ onViewMore, onEdit, onDelete, id, actions }: Props) => {
         </Tooltip>
       )}
       {actions.useEdit && (
-        <Tooltip content='Editar' color='foreground'>
+        <Tooltip
+          content={isInprogress ? actions.labelEditingDisabled : 'Editar'}
+          color='foreground'
+        >
           <span
-            className='text-lg text-default-400 cursor-pointer active:opacity-50'
-            onClick={() => {
-              if (onEdit !== undefined) {
-                onEdit(id)
-              }
-            }}
+            className={cn('text-lg active:opacity-50', {
+              'cursor-not-allowed text-default-300': isInprogress,
+              'cursor-pointer text-default-400': !isInprogress
+            })}
+            onClick={
+              isInprogress
+                ? () => {}
+                : () => {
+                    if (onEdit !== undefined) {
+                      onEdit(id)
+                    }
+                  }
+            }
           >
             <Edit className='w-4 h-4' />
           </span>
         </Tooltip>
       )}
       {actions.useDelete && (
-        <Tooltip color='danger' content={actions?.labelDelete !== undefined ? actions.labelDelete : 'Eliminar'}>
+        <Tooltip
+          color='danger'
+          content={
+            isInprogress
+              ? 'No se puede eliminar mientras está en progreso'
+              : actions?.labelDelete !== undefined
+                ? actions.labelDelete
+                : 'Eliminar'
+          }
+        >
           <span
-            className='text-lg text-danger cursor-pointer active:opacity-50'
-            onClick={() => {
-              if (onDelete !== undefined) {
-                onDelete(id)
-              }
-            }}
+            className={cn('text-lg active:opacity-50', {
+              'cursor-not-allowed text-default-300': isInprogress,
+              'cursor-pointer text-danger': !isInprogress
+            })}
+            onClick={
+              isInprogress
+                ? () => {}
+                : () => {
+                    if (onDelete !== undefined) {
+                      onDelete(id)
+                    }
+                  }
+            }
           >
             {actions.iconDelete !== undefined
               ? (
@@ -78,6 +129,35 @@ const TableActions = ({ onViewMore, onEdit, onDelete, id, actions }: Props) => {
                 )}
           </span>
         </Tooltip>
+      )}
+      {(actions?.isPaymentActions ?? false) && isPendingPayment && (
+        <Tooltip content='Ver Detalles' color='foreground'>
+          <span
+            className='text-lg text-default-400 cursor-pointer active:opacity-50'
+            onClick={() => {
+              if (onDetailPayment !== undefined) {
+                onDetailPayment(row as PaymentsDataType)
+              }
+            }}
+          >
+            <Visible className='w-4 h-4' />
+          </span>
+        </Tooltip>
+      )}
+      {(actions?.isPaymentActions ?? false) && !isPendingPayment && (
+        <div>
+          <Button
+            color='primary'
+            className='bg-cyan-700 text-white h-8 '
+            onClick={() => {
+              if (onConfirmPayment !== undefined) {
+                onConfirmPayment(row as PaymentsDataType)
+              }
+            }}
+          >
+            <span className='text-white'>Confirmar</span>
+          </Button>
+        </div>
       )}
     </div>
   )
