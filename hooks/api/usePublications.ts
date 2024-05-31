829a619130e3dada3784cd435fa759b2021d56a6
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-05-31 09:34:50 -0500

    Feature/ New custom hook for publications

--- hooks/api/usePublications.ts
+++ hooks/api/usePublications.ts
@@ -1,5 +1,14 @@
-import { getBidsAuctionsById, getPublicationById } from '@/helpers/api/publications'
-import { useQuery } from '@tanstack/react-query'
+import {
+  activeOrInactivePublication,
+  getAllAuctionsPublications,
+  getAllGeneralPublications,
+  getBidsAuctionsById,
+  getPublicationById
+} from '@/helpers/api/publications'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { showToast } from '../useToast'
+import { type BodyActivePublication } from '@/types/api/request/publication'
 
 export const useGetPublicationById = (id: number) => {
   return useQuery({
@@ -14,5 +23,43 @@ export const useGetBidAuctionsById = (id: number) => {
     queryKey: ['publicationBids', { id }],
     queryFn: async () => await getBidsAuctionsById(id),
     enabled: id !== 0 && !isNaN(id)
+  })
+}
+
+export const useGetAllAuctionsPublications = ({ page }: { page: number }) => {
+  return useQuery({
+    queryKey: ['publications-auctions', page],
+    queryFn: async () => await getAllAuctionsPublications({ page })
+  })
+}
+
+export const useGetAllGeneralPublications = ({ page }: { page: number }) => {
+  return useQuery({
+    queryKey: ['publications-general', page],
+    queryFn: async () => await getAllGeneralPublications({ page })
+  })
+}
+
+export const useActiveStatusPublication = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['update-status-publication'],
+    mutationFn: async (body: BodyActivePublication) => {
+      return await activeOrInactivePublication(body)
+    },
+    onSuccess: async () => {
+      try {
+        await queryClient.invalidateQueries({
+          queryKey: ['publications-general']
+        })
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      showToast(data.message, 'error')
+    }
   })
 }
