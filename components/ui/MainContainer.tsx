1478a51905b36ff427b4c9f25181d3d11151bd0f
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-06 16:40:16 -0500

    Fix/ MainContent

--- components/ui/MainContainer.tsx
+++ components/ui/MainContainer.tsx
@@ -5,7 +5,7 @@ type MainContainerProps = {
 }
 const MainContainer = ({ children }: MainContainerProps) => {
   return (
-    <div className='w-full bg-white/90 dark:bg-darkBg/50 p-2 md:p-4 rounded-lg border border-gray-500/60 h-full overflow-y-auto'>
+    <div className='w-full bg-white/90 dark:bg-darkBg/50 py-3 px-3 md:py-4 md:px-4 rounded-lg border border-gray-500/60 h-full overflow-y-auto'>
       {children}
     </div>
   )
