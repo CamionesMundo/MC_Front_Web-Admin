3c898a8678e0e2f2dab817ee3e6abbcee5bccef7
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-06 16:55:26 -0500

    Fix/ payments page with filters

--- app/(AdminPanel)/payments-management/payments/page.tsx
+++ app/(AdminPanel)/payments-management/payments/page.tsx
@@ -1,23 +1,28 @@
 'use client'
 import Payments from '@/components/features/payments-management/payments/Payments'
+import CustomPagination from '@/components/table/pagination/CustomPagination'
 import { MainContainer } from '@/components/ui'
 import { useGetAllPayments } from '@/hooks/api/usePayments'
 import useAsyncPagination from '@/hooks/pagination/useAsyncPagination'
-import { Button, Pagination } from '@nextui-org/react'
-import { useQueryClient } from '@tanstack/react-query'
-import React, { useEffect, useMemo, useState } from 'react'
+import useQueryParams from '@/hooks/useQueryParams'
+import { useEffect, useMemo } from 'react'
 
 const PaymentsPage = () => {
-  const queryClient = useQueryClient()
-  const [currentPage, setCurrentPage] = useState<number>(1)
+  const { queryParams } = useQueryParams()
+
   const {
     data: paymentsResponse,
     isLoading,
     isRefetching,
     refetch
   } = useGetAllPayments({
-    page: currentPage
+    page: Number(queryParams.get('page')),
+    pageSize: Number(queryParams.get('pageSize')),
+    query: queryParams.get('query') ?? '',
+    startDate: queryParams.get('startDate') ?? undefined,
+    endDate: queryParams.get('endDate') ?? undefined
   })
+
   const payments = useMemo(() => {
     const payments = paymentsResponse?.data?.payments.map((payment) => ({
       ...payment,
@@ -25,30 +30,15 @@ const PaymentsPage = () => {
     }))
     return payments
   }, [paymentsResponse])
-  const totalPages = useMemo(() => {
-    if (paymentsResponse !== undefined) {
-      return paymentsResponse.data.totalPages
-    }
-    return undefined
-  }, [paymentsResponse])
-
-  const totalRows = useMemo(() => {
-    if (paymentsResponse !== undefined) {
-      return paymentsResponse.data.totalRows
-    }
-    return undefined
-  }, [paymentsResponse])
 
   const { page, onNextPage, onPreviousPage, onChangePage } = useAsyncPagination(
-    { totalPages: totalPages ?? 0 }
+    { totalPages: paymentsResponse?.data?.totalPages ?? 0 }
   )
+
   useEffect(() => {
-    if (totalPages !== undefined) {
-      setCurrentPage(page)
+    if (queryParams !== undefined) {
       refetch()
-        .then(async () => {
-          await queryClient.invalidateQueries({ queryKey: ['payment-orders'] })
-        })
+        .then()
         .catch((error) => {
           console.error(
             'Error al volver a obtener los datos de la subasta:',
@@ -56,53 +46,24 @@ const PaymentsPage = () => {
           )
         })
     }
-  }, [page, totalPages, refetch, queryClient])
-
-  const bottomContent = useMemo(() => {
-    return (
-      <div className='py-2 px-2 flex justify-end items-center'>
-        <Pagination
-          isCompact
-          showControls
-          showShadow
-          color='primary'
-          classNames={{
-            cursor: 'bg-blackText '
-          }}
-          page={page}
-          total={totalPages ?? 0}
-          isDisabled={isLoading}
-          onChange={onChangePage}
-        />
-        <div className='hidden sm:flex w-[30%] justify-end gap-2'>
-          <Button
-            isDisabled={totalPages === 1 || isLoading}
-            size='sm'
-            variant='flat'
-            onPress={onPreviousPage}
-          >
-            Anterior
-          </Button>
-          <Button
-            isDisabled={totalPages === 1 || isLoading}
-            size='sm'
-            variant='flat'
-            onPress={onNextPage}
-          >
-            Siguiente
-          </Button>
-        </div>
-      </div>
-    )
-  }, [isLoading, page, totalPages, onNextPage, onPreviousPage, onChangePage])
+  }, [queryParams, refetch])
 
   return (
     <MainContainer>
       <Payments
         payments={payments ?? []}
         isLoading={isLoading || isRefetching}
-        bottomContent={bottomContent}
-        totalRows={totalRows ?? 0}
+        bottomContent={
+          <CustomPagination
+            page={page}
+            totalPages={paymentsResponse?.data?.totalPages ?? 0}
+            isLoading={isLoading}
+            onChangePage={onChangePage}
+            onNextPage={onNextPage}
+            onPreviousPage={onPreviousPage}
+          />
+        }
+        totalRows={paymentsResponse?.data?.totalRows ?? 0}
       />
     </MainContainer>
   )
