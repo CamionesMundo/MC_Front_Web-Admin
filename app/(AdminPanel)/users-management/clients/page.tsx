d661f7ea89f26531f9d3d931f334458008c17547
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-06 16:53:46 -0500

    Feature/ Added filters in users page

--- app/(AdminPanel)/users-management/clients/page.tsx
+++ app/(AdminPanel)/users-management/clients/page.tsx
@@ -1,27 +1,71 @@
 'use client'
 
 import Users from '@/components/features/users-management/clients/Users'
+import CustomPagination from '@/components/table/pagination/CustomPagination'
 import { MainContainer } from '@/components/ui'
 import { handleClientError } from '@/helpers/error'
 import { useGetAllAppUsers } from '@/hooks/api/useClients'
-import { useMemo } from 'react'
+import useAsyncPagination from '@/hooks/pagination/useAsyncPagination'
+import useQueryParams from '@/hooks/useQueryParams'
+import { useEffect, useMemo } from 'react'
 
 const ClientsPage = () => {
-  const { data: clientsResponse, isLoading } = useGetAllAppUsers()
+  const { queryParams } = useQueryParams()
+
+  const {
+    data: clientsResponse,
+    isLoading,
+    refetch
+  } = useGetAllAppUsers({
+    page: Number(queryParams.get('page')),
+    pageSize: Number(queryParams.get('pageSize')),
+    query: queryParams.get('query') ?? '',
+    userType: Number(queryParams.get('userType'))
+  })
+
   const clients = useMemo(() => {
-    const usersAdmin = clientsResponse?.data?.map((client) => ({
+    const usersAdmin = clientsResponse?.data?.users.map((client) => ({
       ...client,
       id: client.iduser
     }))
     return usersAdmin
   }, [clientsResponse])
+  const { page, onNextPage, onPreviousPage, onChangePage } = useAsyncPagination(
+    { totalPages: clientsResponse?.data?.totalPages ?? 0 }
+  )
+  useEffect(() => {
+    if (queryParams !== undefined) {
+      refetch()
+        .then()
+        .catch((error) => {
+          console.error(
+            'Error al volver a obtener los datos de la subasta:',
+            error
+          )
+        })
+    }
+  }, [queryParams, refetch])
 
   if (clientsResponse?.error !== null && clientsResponse !== undefined) {
     handleClientError(clientsResponse?.statusCode)
   }
   return (
     <MainContainer>
-      <Users isLoading={isLoading} clients={clients ?? []} />
+      <Users
+        isLoading={isLoading}
+        clients={clients ?? []}
+        bottomContent={
+          <CustomPagination
+            page={page}
+            totalPages={clientsResponse?.data?.totalPages ?? 0}
+            isLoading={isLoading}
+            onChangePage={onChangePage}
+            onNextPage={onNextPage}
+            onPreviousPage={onPreviousPage}
+          />
+        }
+        totalRows={clientsResponse?.data.totalRows ?? 0}
+      />
     </MainContainer>
   )
 }
