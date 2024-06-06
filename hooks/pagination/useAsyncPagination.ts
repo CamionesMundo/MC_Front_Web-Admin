fbe5914cbe94f84e258315947432e93ff25f7a37
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-06 16:36:55 -0500

    Feature/ New Custom hook for pagination

--- hooks/pagination/useAsyncPagination.ts
+++ hooks/pagination/useAsyncPagination.ts
@@ -1,27 +1,41 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import useQueryParams from '../useQueryParams'
 
 type Props = {
   totalPages: number
 }
 
 const useAsyncPagination = ({ totalPages }: Props) => {
-  const [page, setPage] = useState(1)
+  const { setQueryParams, queryParams } = useQueryParams()
+  const [page, setPage] = useState(Number(queryParams.get('page')))
 
   const onNextPage = useCallback(() => {
     if (page < totalPages) {
       setPage(page + 1)
+      setQueryParams({ page: page + 1 })
     }
-  }, [page, totalPages])
+  }, [page, totalPages, setQueryParams])
 
   const onPreviousPage = useCallback(() => {
     if (page > 1) {
       setPage(page - 1)
+      setQueryParams({ page: page - 1 })
     }
-  }, [page])
+  }, [page, setQueryParams])
 
-  const onChangePage = useCallback((page: number) => {
-    setPage(page)
-  }, [])
+  const onChangePage = useCallback(
+    (page: number) => {
+      setPage(page)
+      setQueryParams({ page })
+    },
+    [setQueryParams]
+  )
+
+  useEffect(() => {
+    if (queryParams !== undefined) {
+      setPage(Number(queryParams.get('page')))
+    }
+  }, [queryParams])
 
   return {
     page,
