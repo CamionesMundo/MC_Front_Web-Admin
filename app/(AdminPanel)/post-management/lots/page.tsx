7e9a980ac9de36481d001814687cab8d0021884b
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-06 16:54:32 -0500

    Fix/ lot page

--- app/(AdminPanel)/post-management/lots/page.tsx
+++ app/(AdminPanel)/post-management/lots/page.tsx
@@ -1,62 +1,27 @@
 'use client'
 import Lots from '@/components/features/post-management/lots/Lots'
+import CustomPagination from '@/components/table/pagination/CustomPagination'
 import { MainContainer } from '@/components/ui'
 import { useGetAllLots } from '@/hooks/api/useLots'
-import { Button, Pagination, type Selection } from '@nextui-org/react'
-import { useQueryClient } from '@tanstack/react-query'
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import useAsyncPagination from '@/hooks/pagination/useAsyncPagination'
+import useQueryParams from '@/hooks/useQueryParams'
+import { useEffect, useMemo } from 'react'
 
 const LotsPage = () => {
-  const queryClient = useQueryClient()
-
-  const [selectedStatus, setSelectedStatus] = useState<Selection>(
-    new Set(['default'])
-  )
-
-  const [page, setPage] = useState(1)
-
-  const valueStatus = useMemo(() => {
-    if (selectedStatus === undefined) return undefined
-    const statusArray = Array.from(selectedStatus)
-    const value = statusArray[0]
-    return value as string
-  }, [selectedStatus])
-
-  const values = useMemo(() => {
-    if (valueStatus === undefined) {
-      return {
-        status: 'default',
-        page
-      }
-    }
-    return {
-      status: valueStatus,
-      page
-    }
-  }, [valueStatus, page])
+  const { queryParams } = useQueryParams()
 
   const {
     data: lotsResponse,
     isLoading,
-    refetch,
-    isRefetching
-  } = useGetAllLots(values.status, values.page)
-
-  useEffect(() => {
-    if (valueStatus === undefined) {
-      setPage(1)
-    }
-  }, [valueStatus])
-
-  const handleSelectionChange = useCallback(
-    async (keys: Selection) => {
-      setSelectedStatus(keys)
-      setPage(1)
-      await queryClient.invalidateQueries({ queryKey: ['lots'] })
-      await refetch()
-    },
-    [refetch, queryClient]
-  )
+    refetch
+  } = useGetAllLots({
+    page: Number(queryParams.get('page')),
+    pageSize: Number(queryParams.get('pageSize')),
+    query: queryParams.get('query') ?? '',
+    status: queryParams.get('status') ?? '',
+    startDate: queryParams.get('startDate') ?? undefined,
+    endDate: queryParams.get('endDate') ?? undefined
+  })
 
   const lots = useMemo(() => {
     const agents = lotsResponse?.data?.lots.map((lot) => ({
@@ -66,40 +31,12 @@ const LotsPage = () => {
     return agents
   }, [lotsResponse])
 
-  const pages = useMemo(() => {
-    if (lotsResponse !== undefined) {
-      return lotsResponse.data?.totalPages
-    } else {
-      return 0
-    }
-  }, [lotsResponse])
-
-  const totalRows = useMemo(() => {
-    if (lotsResponse !== undefined) {
-      return lotsResponse.data?.totalRows
-    } else {
-      return 0
-    }
-  }, [lotsResponse])
-
-  const onNextPage = useCallback(() => {
-    if (page < pages) {
-      setPage(page + 1)
-    }
-  }, [page, pages])
-
-  const onPreviousPage = useCallback(() => {
-    if (page > 1) {
-      setPage(page - 1)
-    }
-  }, [page])
-
-  const onChangePage = useCallback((page: number) => {
-    setPage(page)
-  }, [])
+  const { page, onNextPage, onPreviousPage, onChangePage } = useAsyncPagination(
+    { totalPages: lotsResponse?.data?.totalPages ?? 0 }
+  )
 
   useEffect(() => {
-    if (page !== 0) {
+    if (queryParams !== undefined) {
       refetch()
         .then()
         .catch((error) => {
@@ -109,55 +46,24 @@ const LotsPage = () => {
           )
         })
     }
-  }, [page, refetch])
-
-  const bottomContent = useMemo(() => {
-    return (
-      <div className='py-2 px-2 flex justify-end items-center'>
-        <Pagination
-          isCompact
-          showControls
-          showShadow
-          color='primary'
-          classNames={{
-            cursor: 'bg-blackText '
-          }}
-          page={page}
-          total={pages}
-          isDisabled={isLoading}
-          onChange={onChangePage}
-        />
-        <div className='hidden sm:flex w-[30%] justify-end gap-2'>
-          <Button
-            isDisabled={pages === 1 || isLoading}
-            size='sm'
-            variant='flat'
-            onPress={onPreviousPage}
-          >
-            Anterior
-          </Button>
-          <Button
-            isDisabled={pages === 1 || isLoading}
-            size='sm'
-            variant='flat'
-            onPress={onNextPage}
-          >
-            Siguiente
-          </Button>
-        </div>
-      </div>
-    )
-  }, [isLoading, page, pages, onNextPage, onPreviousPage, onChangePage])
+  }, [queryParams, refetch])
 
   return (
     <MainContainer>
       <Lots
         lots={lots ?? []}
-        isLoading={isLoading || isRefetching}
-        selectedStatus={selectedStatus}
-        handleSelectionChange={handleSelectionChange}
-        bottomContent={bottomContent}
-        totalRows={totalRows}
+        isLoading={isLoading}
+        bottomContent={
+          <CustomPagination
+            page={page}
+            totalPages={lotsResponse?.data?.totalPages ?? 0}
+            isLoading={isLoading}
+            onChangePage={onChangePage}
+            onNextPage={onNextPage}
+            onPreviousPage={onPreviousPage}
+          />
+        }
+        totalRows={lotsResponse?.data?.totalRows ?? 0}
       />
     </MainContainer>
   )
