fdeedcbb8bfce02a90f11b0313712b5eb3935456
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-13 09:22:31 -0500

    Feature/ new columns for order table

---
+++ const/columns/shipments.ts
@@ -0,0 +1,30 @@
+export const ordersFiltersColumns = [
+  {
+    key: 'order_title',
+    display: 'ID/ Titulo'
+  },
+  {
+    key: 'origin',
+    display: 'Origen Venta'
+  },
+  {
+    key: 'order_country',
+    display: 'País'
+  },
+  {
+    key: 'order_price',
+    display: 'Precio Venta'
+  },
+  {
+    key: 'order_date',
+    display: 'Fecha Venta'
+  },
+  {
+    key: 'order_status',
+    display: 'Status'
+  },
+  {
+    key: 'actions',
+    display: 'Acciones'
+  }
+]
