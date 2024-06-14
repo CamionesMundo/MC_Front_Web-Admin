e791d41cdd6408f43c3c8b828b696193f4365728
Eichtici-Dev <henry.tirado@ingeniust.com>
2024-06-14 18:53:31 -0500

    Feature/ new Custom hook for orders details api

--- hooks/api/useOrders.ts
+++ hooks/api/useOrders.ts
@@ -1,10 +1,20 @@
 import {
+  cancelOrder,
   getAllOrders,
   getOrderById,
-  getOrderStatus
+  getOrderStatus,
+  updateHistoryStatus,
+  updateOrder
 } from '@/helpers/api/orders'
-import { type OrderFilter } from '@/types/api/request/order'
-import { useQuery } from '@tanstack/react-query'
+import {
+  type RequestUpdateOrder,
+  type BodyHistoryStatus,
+  type OrderFilter,
+  type RequestDeleteOrder
+} from '@/types/api/request/order'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
+import { showToast } from '../useToast'
 
 export const useGetAllOrders = ({
   page,
@@ -40,5 +50,81 @@ export const useGetOrderById = (id: number) => {
     queryKey: ['order-admin', { id }],
     queryFn: async () => await getOrderById(id),
     enabled: id !== 0 && !isNaN(id)
+  })
+}
+
+export const useUpdateHistoryStatus = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['updateHistory'],
+    mutationFn: async (body: BodyHistoryStatus) => {
+      return await updateHistoryStatus(body)
+    },
+    onSuccess: async (response) => {
+      try {
+        await queryClient.invalidateQueries({ queryKey: ['orders-admin'] })
+        await queryClient.invalidateQueries({
+          queryKey: ['order-admin', { id: response?.data.idorder }]
+        })
+
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      showToast(data.message, 'error')
+    }
+  })
+}
+
+export const useUpdateOrder = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['updateOrder'],
+    mutationFn: async (request: RequestUpdateOrder) => {
+      return await updateOrder(request.id, request.body)
+    },
+    onSuccess: async (response) => {
+      try {
+        await queryClient.invalidateQueries({ queryKey: ['orders-admin'] })
+        await queryClient.invalidateQueries({
+          queryKey: ['order-admin', { id: response?.data.idorder }]
+        })
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      showToast(data.message, 'error')
+    }
+  })
+}
+
+export const useCancelOrder = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  return useMutation({
+    mutationKey: ['cancelOrder'],
+    mutationFn: async (request: RequestDeleteOrder) => {
+      return await cancelOrder(request.id, request.body)
+    },
+    onSuccess: async (response) => {
+      try {
+        await queryClient.invalidateQueries({ queryKey: ['orders-admin'] })
+        await queryClient.invalidateQueries({
+          queryKey: ['order-admin', { id: response?.data.idorder }]
+        })
+        router.refresh()
+      } catch (error) {
+        console.error('Error al invalidar las queries:', error)
+      }
+    },
+    onError: (data: Error) => {
+      showToast(data.message, 'error')
+    }
   })
 }
